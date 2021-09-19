fc0f8cc553151ea59a9af686cfbe3d3a45c7759a
Michael Amiro <odindo.michael@gmail.com>
2021-09-19 11:16:14 +0300

    Initial Commit

---
+++ Settings/SettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsApp.swift
+//  Settings
+//
+//  Created by Michael Amiro on 19/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
